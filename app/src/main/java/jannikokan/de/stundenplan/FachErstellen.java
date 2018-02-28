54028a87d941a97326454968991dfbd56798b07f
JDR3 <jannikdreier@web.de>
2018-02-28 19:11:03 +0100

    Activty zum Fach erstellen. Immer noch Errors aber funktioniert

---
+++ app/src/main/java/jannikokan/de/stundenplan/FachErstellen.java
@@ -0,0 +1,27 @@
+package jannikokan.de.stundenplan;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+/**
+ * Created by jannik on 27.02.2018.
+ */
+
+public class FachErstellen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fach_erstellen_activity);
+    }
+
+
+
+}
+
+
+

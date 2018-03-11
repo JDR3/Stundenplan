5fbbe76341e19196336fcd60bf5f54a76b6b1bcb
Okan <businesss187@gmail.com>
2018-03-11 17:17:38 +0100

    10_03_2018_

--- app/src/main/java/jannikokan/de/stundenplan/SliderActivityActivity.java
+++ app/src/main/java/jannikokan/de/stundenplan/SliderActivityActivity.java
@@ -1,5 +1,6 @@
 package jannikokan.de.stundenplan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,8 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class SliderActivityActivity extends AppCompatActivity
+
+
+
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -22,6 +27,14 @@ public class SliderActivityActivity extends AppCompatActivity
         setContentView(R.layout.slider_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,7 +96,8 @@ public class SliderActivityActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Intent i = new Intent(SliderActivityActivity.this,FachErstellen.class);
+            startActivity(i);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {

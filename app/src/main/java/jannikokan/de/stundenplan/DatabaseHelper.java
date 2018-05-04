c69bb26d41dc81cab1b8e7bcf24e889a10ac387e
Okan <businesss187@gmail.com>
2018-05-05 01:43:49 +0200

    hallo

--- app/src/main/java/jannikokan/de/stundenplan/DatabaseHelper.java
+++ app/src/main/java/jannikokan/de/stundenplan/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Jannik on 14.03.2018.
@@ -20,37 +21,49 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_4 = "FACHRAUM";
     public static final String COL_5 = "FACHLEHRER";
     public static final String COL_6 = "FACHFARBE";
-
-
-    public static final String TABLE_NAME2 = "Lehrer_table";
-    public static final String COL_7 = "ID_LEHRER"
-    public static final String COL_8 = "LEHRERNAME";
-    public static final String COL_9 = "LEHRERKUERZEL";
-    public static final String COL_10 = "LEHRERRAUM";
-    public static final String COL_11 = "LEHRERMAIL";
-
+    public static final String TABLE_LEHRER = "Lehrer_table";
+    public static final String LEHRERID = "ID_L";
+    public static final String LEHRERNAME = "LEHRERNAME";
+    public static final String LEHRERKUERZEL = "LEHRERKUERZEL";
+    public static final String LEHRERRAUM = "LEHRERRAUM";
+    public static final String LEHRERMAIL = "LEHRERMAIL";
 
 
 
 
-
+private static final String create_Table2 = "create table " + TABLE_LEHRER + "("+ LEHRERID +"INTEGER PRIMARY KEY," + LEHRERNAME +"TEXT," + LEHRERKUERZEL + "TEXT,"+ LEHRERRAUM + "TEXT," + LEHRERMAIL + "TEXT)";
+private static final String create_Table =  "create table " + TABLE_NAME + "("+COL_1+ "INTEGER PRIMARY KEY," + COL_2 +"TEXT," + COL_3+ " TEXT,"+  COL_4+ "TEXT," + COL_5+ " TEXT)";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
 
+        Log.d("MeineAPP", "DB angelegt");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, FACHNAME TEXT, FACHKUERZEL TEXT, FACHRAUM TEXT, FACHLEHRER TEXT)");
+        Log.d("MeineAPP", "Tabelle angelegt");
+      //  db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, FACHNAME TEXT, FACHKUERZEL TEXT, FACHRAUM TEXT, FACHLEHRER TEXT)");
+        db.execSQL(create_Table);
+        db.execSQL(create_Table2);
 
     }
 
+
+
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+
+       // db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME );
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL( "DROP TABLE IF EXISTS " + TABLE_LEHRER);
+
         onCreate(db);
+        Log.d("MeineAPP", "in upgrade");
+
     }
 
     public boolean speichereFach(String fachName, String fachKuerzel, String fachRaum, String fachLehrer){
@@ -71,8 +84,40 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public Cursor zeigeFaecher(){
+
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor res = db.rawQuery("select *from "+TABLE_NAME,null);
         return res;
     }
+
+
+    public boolean speicherLehrer(String lehrerName, String lehrerKuerzel, String lehrerRaum, String lehrerMail){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(LEHRERNAME, lehrerName);
+        contentValues.put(LEHRERKUERZEL, lehrerKuerzel);
+        contentValues.put(LEHRERRAUM, lehrerRaum);
+        contentValues.put(LEHRERMAIL, lehrerMail);
+       long result = db.insert(TABLE_LEHRER, null, contentValues);
+        if (result == -1){
+            return  false;
+        }
+        else {
+            return  true;
+        }
+
+    }
+
+
+    public Cursor zeigeLehrer(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select *from "+ TABLE_LEHRER,null);
+        return res;
+    }
+
+
+
+
 }
+
+
--- app/src/main/java/jannikokan/de/stundenplan/LehrerErstellen.java
+++ app/src/main/java/jannikokan/de/stundenplan/LehrerErstellen.java
@@ -1,13 +1,21 @@
 package jannikokan.de.stundenplan;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LehrerErstellen extends AppCompatActivity {
 
-
+    DatabaseHelper myDb;
     EditText editTextLehrerName;
     EditText editTextLehrerKuerzel;
     EditText editTextLehrerMail;
@@ -23,6 +31,74 @@ public class LehrerErstellen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lehrer_erstellen_activity);
 
+        myDb = new DatabaseHelper(this);
+        editTextLehrerName = (EditText) findViewById(R.id.editTextLehrerName);
+        editTextLehrerKuerzel = (EditText) findViewById(R.id.editTextLehrerKuerzel);
+        editTextLehrerRaum = (EditText) findViewById(R.id.editTextLehrerRaum);
+        editTextLehrerMail = (EditText) findViewById(R.id.editTextLehrerMail);
+        buttonLehrerSpeichern = (Button) findViewById(R.id.buttonLehrerSpeichern);
+        buttonLehrerAnzeigen = (Button) findViewById(R.id.buttonLehrerAnzeigen);
+        addLehrer();
+        zeigeLehrer();
 
     }
+
+
+
+    public void addLehrer(){
+        buttonLehrerSpeichern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean istGespeichert = myDb.speicherLehrer(editTextLehrerName.getText().toString(),
+                        editTextLehrerKuerzel.getText().toString(),
+                        editTextLehrerRaum.getText().toString(),
+                        editTextLehrerMail.getText().toString());
+                if (istGespeichert==true){
+                    Toast.makeText(LehrerErstellen.this, "Lehrer wurde gespeichert.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(LehrerErstellen.this, "Lehrer konnte nicht gespeichert werden.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+
+
+
+    public void zeigeLehrer() {
+        buttonLehrerAnzeigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res =myDb.zeigeLehrer();
+                if (res.getCount() == 0){
+                    showToast("Fehler", "Keinen Lehrer gefunden");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()) {
+                    buffer.append("ID:" + res.getString(0)+ "\n");
+                    buffer.append("Lehrer" + res.getString( 1) + "\n");
+                    buffer.append("Kuerzel" + res.getString(2) + "\n");
+                    buffer.append("Raum" + res.getString(3) + "\n");
+                    buffer.append("Mail" + res.getString(4) + "\n\n");
+                }
+                showToast("Lehrer", buffer.toString());
+
+            }
+        });
+    }
+
+
+    public void showToast(String title, String Nachricht){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Nachricht);
+        builder.show();
+    }
+
+
+
 }
